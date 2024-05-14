f8366e5d1383bdbeddb85a4ccdcd824ced7e743d
Ryan <RyanABurkhardt@gmail.com>
2024-05-14 07:07:39 -0400

    Setting up Problem 1

---
+++ Homework1/Problem1.java
@@ -0,0 +1,12 @@
+package Homework1;
+
+class Problem1 {
+
+    int square(int n) { 
+        int result = 0; 
+        for (int i = 1; i <= n; i++) 
+        result += 2 * i - 1; 
+        return result; 
+        } 
+        
+}
