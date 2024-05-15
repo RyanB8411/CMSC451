82a843bc9dc29d73eca933163df8578be3dd1718
Ryan <RyanABurkhardt@gmail.com>
2024-05-15 05:13:17 -0400

    Working on Homework Problem 1

--- Homework1/Problem1.java
+++ Homework1/Problem1.java
@@ -2,11 +2,17 @@ package Homework1;
 
 class Problem1 {
 
+    int n = 0;
+
     int square(int n) { 
         int result = 0; 
-        for (int i = 1; i <= n; i++) 
+        for (int i = 1; i <= n; i++)
         result += 2 * i - 1; 
         return result; 
         } 
-        
+    public static void main(String[] args) {
+        Problem1 p1 = new Problem1();
+        p1.n = 2;
+        System.out.println(p1.square(p1.n));
+    }
 }
