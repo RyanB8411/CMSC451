5c846576b8810f333a5f89b4ce00c4ac9d5f5c88
Ryan <RyanABurkhardt@gmail.com>
2024-05-15 05:27:20 -0400

    Problem 1 Finished

--- Homework1/Problem1.java
+++ Homework1/Problem1.java
@@ -10,9 +10,38 @@ class Problem1 {
         result += 2 * i - 1; 
         return result; 
         } 
+
+        /**
+        * Returns the square of a given integer n.
+        * 
+        * Preconditions: n >= 0
+        * Postconditions: returns the square of n
+        */
+    int recSquare(int n) {
+        // Base case: 0 squared is 0
+        if (n == 0) {
+            return 0;
+        } else {
+        // Recursive case: n squared is (n-1) squared plus 2n - 1
+            return recSquare(n - 1) + 2 * n - 1;
+        }
+    }
+    
     public static void main(String[] args) {
         Problem1 p1 = new Problem1();
         p1.n = 2;
         System.out.println(p1.square(p1.n));
+        Problem1 p2 = new Problem1();
+        p2.n = 6;
+        System.out.println(p2.square(p2.n));
+        Problem1 p3 = new Problem1();
+        p3.n = 0;
+        System.out.println(p3.recSquare(p3.n));
+        Problem1 p4 = new Problem1();
+        p4.n = 2;
+        System.out.println(p4.recSquare(p4.n));
+        Problem1 p5 = new Problem1();
+        p5.n = 6;
+        System.out.println(p5.recSquare(p5.n));
     }
 }
